0acae7a9ba410fd15a846c7b8b7b04cdc75a8c28
grantkee <grant.d.kee@gmail.com>
2019-08-31 23:23:10 -0500

    the initial prompt works. now i need to figure out how to seperate the user input and do math on the string

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const readline = require('readline')
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Input math equation: ', (answer) => {
+    console.log(`Calculating ${answer}= ...`);
+    doMath(answer);
+
+    rl.close();
+});
+
+function doMath(answer) {}
